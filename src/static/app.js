164e179e699604980023abe051f657f867982077
Ruben Garcia <ruben.garcia@sevenval.com>
2017-06-30 10:18:40 +0200

    Change naming and order of output markup

--- src/static/app.js
+++ src/static/app.js
@@ -1,28 +1,27 @@
-function inner(value) {
+function innerCheck(value) {
   return Object.keys(value).map((key) => {
     const className = value[key] ? 'available' : 'taken'
     return `<li class=${className}>${key}</li>`
   }).join('')
 }
 
-function inner2(value) {
+function renderCheck(result) {
+  $('#checkOutput').html(Object.keys(result).map((key) => {
+    return `<li>${key}<ul>${innerCheck(result[key])}</ul></li>`
+  }).join(''))
+}
+
+function innerSuggestion(value) {
   return Object.values(value).map((key) => {
     return `<li>${key}</li>`
   }).join('')
 }
 
-function render(result) {
-  $('#output').html(Object.keys(result).map((key) => {
-    return `<li>${key}<ul>${inner(result[key])}</ul></li>`
+function renderSuggestions(result) {
+  $('#suggestionsOutput').html(Object.keys(result).map((value) => {
+    return `<li class="suggestionList"><ul>${innerSuggestion(result[value])}</ul></li>`
   }).join(''))
 }
-
-function render2(result) {
-  $('#output2').html(Object.keys(result).map((value) => {
-    return `<li>${value}<ul>${inner2(result[value])}</ul></li>`
-  }).join(''))
-}
-
 
 function getNames() {
   $.get('/v1/name-ideas').then((result) => {
@@ -41,23 +40,23 @@ $('#ideas').on('click', 'li', (event) => {
 
 $('#check').on('submit', (event) => {
   event.preventDefault()
-  $('#output').html('<li>loading...</li>')
+  $('#checkOutput').html('<li>loading...</li>')
   const url = $(event.target).attr('action')
   const bandname = $(event.target).find('[name=bandname]').val()
   $.get(url, {
     bandname,
-  }).then(render)
+  }).then(renderCheck)
 })
 
 $('#names').on('submit', (event) => {
   event.preventDefault()
-  $('#output2').html('<li>loading...</li>')
+  $('#suggestionsOutput').html('<li>loading...</li>')
   const url = $(event.target).attr('action')
   const bandname = $(event.target).find('[name=bandname]').val()
   $.get(url, {
     bandname,
-  }).then(render2)
+  }).then(renderSuggestions)
 })
 
 // const fixture = JSON.parse('{"domains":{"com":false,"org":false,"net":true,"info":false},"socials":{"facebook":true,"twitter":false,"github":false}}')
-// render(fixture)
+// renderCheck(fixture)
