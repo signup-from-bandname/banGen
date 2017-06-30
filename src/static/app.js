fa002b3defbe82fafc61622834ecae82e8eda5a2
readonemohamed <readone.mohamed@sevenval.com>
2017-06-30 03:05:58 +0200

    Wordgen (#11)
    * Aktueller Stand
    
    test
    
    * korrekturen
    
    classname

--- src/static/app.js
+++ src/static/app.js
@@ -1,9 +1,13 @@
-
-
 function inner(value) {
   return Object.keys(value).map((key) => {
     const className = value[key] ? 'available' : 'taken'
     return `<li class=${className}>${key}</li>`
+  }).join('')
+}
+
+function inner2(value) {
+  return Object.values(value).map((key) => {
+    return `<li>${key}</li>`
   }).join('')
 }
 
@@ -12,6 +16,13 @@ function render(result) {
     return `<li>${key}<ul>${inner(result[key])}</ul></li>`
   }).join(''))
 }
+
+function render2(result) {
+  $('#output2').html(Object.keys(result).map((value) => {
+    return `<li>${value}<ul>${inner2(result[value])}</ul></li>`
+  }).join(''))
+}
+
 
 function getNames() {
   $.get('/v1/name-ideas').then((result) => {
@@ -28,7 +39,7 @@ $('#ideas').on('click', 'li', (event) => {
   $('#names input[name=bandname]').val($(event.target).text())
 })
 
-$('form').on('submit', (event) => {
+$('#check').on('submit', (event) => {
   event.preventDefault()
   $('#output').html('<li>loading...</li>')
   const url = $(event.target).attr('action')
@@ -36,6 +47,16 @@ $('form').on('submit', (event) => {
   $.get(url, {
     bandname,
   }).then(render)
+})
+
+$('#names').on('submit', (event) => {
+  event.preventDefault()
+  $('#output2').html('<li>loading...</li>')
+  const url = $(event.target).attr('action')
+  const bandname = $(event.target).find('[name=bandname]').val()
+  $.get(url, {
+    bandname,
+  }).then(render2)
 })
 
 // const fixture = JSON.parse('{"domains":{"com":false,"org":false,"net":true,"info":false},"socials":{"facebook":true,"twitter":false,"github":false}}')
